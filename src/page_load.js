47d7b76f3f347a172bb4b890b10d65f9789ec431
Joseph McCann <joe.mccann@mailbox.org>
2022-10-26 17:12:03 -0400

    Use a container inside the content div. Setup other nav links.

--- src/page_load.js
+++ src/page_load.js
@@ -2,28 +2,51 @@ import mainImage from './images/restaurant.jpg'
 
 export default function pageLoad(content) {
 
-  const welcomeHeader = document.createElement('h1');
-  welcomeHeader.textContent = "Welcome to the Restaurant";
-  welcomeHeader.classList.add('header');
-  content.appendChild(welcomeHeader);
+  const container = document.createElement('div');
+  container.classList.add('container');
 
-  const nav = document.createElement('div');
+  const nav = document.createElement('nav');
   const homeLink = document.createElement('a');
   homeLink.textContent = 'Home'
   homeLink.setAttribute('id', 'home')
   homeLink.href = '#';
   nav.appendChild(homeLink);
+  const menuLink = document.createElement('a');
+  menuLink.textContent = 'Menu';
+  menuLink.setAttribute('id', 'menu');
+  menuLink.href = '#';
+  nav.appendChild(menuLink);
+  const contactLink = document.createElement('a');
+  contactLink.textContent = 'Contact';
+  contactLink.setAttribute('id', 'contact');
+  contactLink.href = '#'
+  nav.appendChild(contactLink);
+
   content.appendChild(nav);
+
+
+  const welcomeHeader = document.createElement('h1');
+  welcomeHeader.textContent = "The Veggie Shack";
+  welcomeHeader.classList.add('header');
+  content.appendChild(welcomeHeader);
 
   const image = new Image();
   image.src = mainImage;
   image.classList.add('image');
-  content.appendChild(image);
+  container.appendChild(image);
 
   const copy = document.createElement('p');
-  copy.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+  copy.textContent = `
+    The Veggie Shack has provided the diners of New England with
+    nutritious and delicious, plant-based meals since 1998. The menu listed here is our base menu,
+    but you can call or stop in for our inspirational lunch and dinner
+    specials! Click on the 'Contact' tab above to give us a call. 
+    Thank you for visiting our humble website, see you soon!
+  `
   copy.classList.add('copy');
-  content.appendChild(copy);
+  container.appendChild(copy);
+
+  content.appendChild(container);
 
   return content;
 }
