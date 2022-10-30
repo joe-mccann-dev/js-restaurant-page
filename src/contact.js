6106bd93ac75554d942eff68305a9e5faa5c8b4f
Joseph McCann <joe.mccann@mailbox.org>
2022-10-29 20:52:47 -0400

    Improve styling of contact page.

--- src/contact.js
+++ src/contact.js
@@ -1,24 +1,28 @@
 export default function showContacts(staticDiv, currentModule) {
   const contacts = [
     {
-      name: 'catering',
-      detail: '555-555-1234'
-    },
-    {
       name: 'general inquiries',
       detail: '555-555-5678'
     },
     {
-      name: 'address',
-      detail: '989 Garden Boulevard, Somewhere, New England 22112'
+      name: 'catering',
+      detail: '555-555-1234'
     },
     {
       name: 'email',
       detail: 'info@veggieshack.com'
-    }
+    },
+    {
+      name: 'address',
+      detail: '988 Garden Boulevard, Metrowest 55555'
+    },
 
   ]
   const contactContainer = document.createElement('div');
+  const contactHeader = document.createElement('h2');
+  contactHeader.textContent = 'Contact Us'
+  contactHeader.classList.add('contact_header');
+  contactContainer.appendChild(contactHeader);
   const contactList = document.createElement('ul');
 
   contacts.forEach((contact) => {
@@ -39,10 +43,12 @@ function createListing(contact) {
   const listing = document.createElement('li');
 
   const name = document.createElement('h3');
+  name.classList.add('contact_name');
   name.textContent = contact['name'];
   listing.appendChild(name);
 
   const detail = document.createElement('p');
+  detail.classList.add('contact_detail');
   detail.textContent = contact['detail'];
   listing.appendChild(detail);
 
